fca415336310fc58f1eabb12ef61e8060d0cdbf9
Isaac Gums Bodenmuller <isaac.gums@havan.com.br>
2026-01-06 12:33:10 -0300

    adicionado botões de filtro para o grid das transações.

--- src/components/elements/InputOption.jsx
+++ src/components/elements/InputOption.jsx
@@ -1,11 +1,31 @@
 import { useEffect, useRef, useState } from "react";
 import { ChevronDown } from "lucide-react";
 
-export default function InputOption({ title, options, value, onChange }) {
+export default function InputOption({
+  title,
+  options,
+  value,
+  onChange,
+  isTitle = true,
+}) {
+  const wrapperRef = useRef(null);
   const buttonRef = useRef(null);
   const [openUp, setOpenUp] = useState(false);
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+
+    if (open) document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [open]);
   useEffect(() => {
     if (!open || !buttonRef.current) return;
 
@@ -13,16 +33,24 @@ export default function InputOption({ title, options, value, onChange }) {
     const spaceBelow = window.innerHeight - rect.bottom;
 
     setOpenUp(spaceBelow < 260);
-  }, [open]);
+  }, [open, options.length]);
 
-  const selected = options.find((opt) => opt.id === value);
+  const selected = options.find((opt) => opt.id === value) || null;
+  const hasAllOptions = options.some((opt) => opt.id === 0);
+
+  const borderOptionSelected = (index) => {
+    if (index === 0) return "rounded-t-lg";
+    if (index === options.length - 1) return "rounded-b-lg";
+    return "rounded";
+  };
 
   return (
-    <div className="relative w-full">
-      <label className="bg-white absolute -top-2 left-2 h-5 w-fit text-left px-4 text-slate-500 text-xs">
-        {title}
-      </label>
-      {/* Botão */}
+    <div ref={wrapperRef} className="relative w-full">
+      {isTitle && (
+        <label className="bg-white absolute -top-2 left-2 h-5 w-fit text-left px-4 text-slate-500 text-xs">
+          {title}
+        </label>
+      )}
       <button
         ref={buttonRef}
         type="button"
@@ -32,37 +60,43 @@ export default function InputOption({ title, options, value, onChange }) {
         <div className="flex items-center gap-3 h-3">
           {selected ? (
             <>
-              <span className="text-lg">{selected.icon}</span>
+              {selected.icon && (
+                <span className="text-lg">{selected.icon}</span>
+              )}
               <span className="text-sm">{selected.description}</span>
             </>
           ) : (
-            <span className="text-slate-400 text-sm">
-              Selecione uma categoria
-            </span>
+            !hasAllOptions && (
+              <span className="text-slate-400 text-sm">
+                Selecione uma categoria
+              </span>
+            )
           )}
         </div>
         <ChevronDown className="size-4 text-slate-500" />
       </button>
 
-      {/* Lista */}
       {open && (
         <div
           className={`
       absolute z-20 w-full bg-white border rounded-xl shadow-lg
-      max-h-64 overflow-y-auto
+      max-h-64 overflow-y-auto px-2 py-2
       ${openUp ? "bottom-full mb-2" : "top-full mt-2"}
     `}
         >
-          {options.map((opt) => (
+          {options.map((opt, index) => (
             <div
               key={opt.id}
               onClick={() => {
                 onChange(opt.id);
                 setOpen(false);
               }}
-              className="flex items-center gap-3 px-4 py-2 cursor-pointer transition hover:bg-slate-100 text-slate-700"
+              className={`flex items-center gap-3 px-4 py-2 cursor-pointer transition hover:bg-slate-100 text-slate-700
+                ${selected?.id === opt.id ? "bg-slate-200" : ""}
+                ${borderOptionSelected(index)}
+                `}
             >
-              <span className="text-lg">{opt.icon}</span>
+              {opt.icon && <span className="text-lg">{opt.icon}</span>}
               <span className="text-sm">{opt.description}</span>
             </div>
           ))}
@@ -71,31 +105,3 @@ export default function InputOption({ title, options, value, onChange }) {
     </div>
   );
 }
-
-// export default CategorySelect;
-
-// function InputOption({ title, options, value, onChange }) {
-//   return (
-//     <div className="flex flex-col relative">
-//       <label className="bg-white absolute -top-2 left-2 h-5 w-fit text-left px-4 text-slate-500 text-xs">
-//         {title}
-//       </label>
-
-//       <select
-//         className="rounded-xl px-4 py-2 pt-3 bg-white border border-slate-200 outline-slate-300 text-slate-400 font-light"
-//         value={value}
-//         onChange={onChange}
-//       >
-//         <option value="" disabled className="border-slate-200">
-//           Select an option
-//         </option>
-
-//         {options.map((option) => (
-//           <option key={option.id} value={option.id}>
-//             {option.icon} {option.description}
-//           </option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-// }
--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -1,7 +1,59 @@
-export default function TransactionPage({ /*expenses,*/ pages, onGoPage }) {
+import { Plus } from "lucide-react";
+import { useState, useEffect } from "react";
+import Title from "../components/elements/Title";
+import TextWithIcon from "../components/elements/TextWithIcon";
+import Button from "../components/elements/Button";
+import InputSearch from "../components/elements/InputSearch";
+import InputOption from "../components/elements/InputOption";
+
+export default function TransactionPage({
+  // expenses,
+  options,
+  pages,
+  onGoPage,
+  onModalAction,
+}) {
+  const [category, setCategory] = useState(0);
+  const [type, setType] = useState(0);
+
+  const [typeOptions] = useState([
+    {
+      id: 0,
+      description: "Todos",
+    },
+    {
+      id: 1,
+      description: "Receitas",
+    },
+    {
+      id: 2,
+      description: "Despesas",
+    },
+  ]);
+
+  const categoryOptions = options.some((opt) => opt.id === 0)
+    ? options
+    : [
+        { id: 0, icon: "", description: "Todas as categorias", isOther: true },
+        ...options,
+      ];
+
+  const filteredCategoryOptions = categoryOptions.filter((option) => {
+    if (option.id === 0 || option.isOther) return true;
+    if (type === 1) return option.isExpense === false;
+    if (type === 2) return option.isExpense === true;
+    return true;
+  });
+
+  useEffect(() => {
+    if (!filteredCategoryOptions.some((opt) => opt.id === category)) {
+      setCategory(0);
+    }
+  }, [type, filteredCategoryOptions, category]);
+
   return (
-    <div className="flex flex-col h-full pr-4 relative py-4 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 overflow-y-auto">
-      <div className="flex items-center gap-4 pt-2">
+    <div className="flex flex-col h-full pr-4 relative py-4 pt-6 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 overflow-y-auto gap-8">
+      <div>
         <div className={`text-sm text-slate-500 cursor-pointer`}>
           {pages.map((page, index) => (
             <span
@@ -20,6 +72,42 @@ export default function TransactionPage({ /*expenses,*/ pages, onGoPage }) {
             </span>
           ))}
         </div>
+
+        <div className="flex flex-col justify-center gap-8 pt-4">
+          <div className="flex flex-col gap-2">
+            <div>
+              <Title size="xl" weight="medium" position="start">
+                Home
+              </Title>
+              <span className="text-slate-500 text-base">
+                Visão geral das suas finanças
+              </span>
+            </div>
+            <Button
+              onClick={() => onModalAction(true, "expense")}
+              color="darkGray"
+            >
+              <TextWithIcon iconSize="4" side="left" icon={Plus}>
+                Nova Transação
+              </TextWithIcon>
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col border border-slate-200 w-full h-50 rounded-xl shadow-sm p-4 gap-4 justify-center">
+        <InputSearch placeholder="Buscar transações..." />
+        <InputOption
+          options={typeOptions}
+          value={type}
+          onChange={(id) => setType(id)}
+          isTitle={false}
+        />
+        <InputOption
+          options={filteredCategoryOptions}
+          value={category}
+          onChange={(id) => setCategory(id)}
+          isTitle={false}
+        />
       </div>
     </div>
   );
