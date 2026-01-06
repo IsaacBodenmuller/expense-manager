9e077fc0cfd26fda90688dcdf1f87ad330f4a006
IsaacBodenmuller <isaac23gb@gmail.com>
2026-01-06 12:39:42 -0300

    verificando usuário do commit.

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
   const [pages, setPages] = useState(["Home"]);
   const [editingGoal, setEditingGoal] = useState(null);
 
+  //home
   const handleGoPage = (page) => {
     setCurrentPage(page);
     setIsOpenMenu(false);
